ff6748ce81fdb960b7ddb9bd44c37579fb86021f
retvain <retvain@gmail.com>
2026-09-20 16:53:11 +0500

    Set JetBrains Mono font size

--- wezterm.lua
+++ wezterm.lua
@@ -23,6 +23,8 @@ config.color_schemes = {
   },
 }
 config.color_scheme = 'Rosé Pine (Gogh)'
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 11.0
 
 -- Alt+Shift+D: split the current pane and open PowerShell 7 in the new pane.
 -- `phys:D` refers to the physical D-key, so this works with a Russian layout too.
