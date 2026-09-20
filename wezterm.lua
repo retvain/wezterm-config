a3f44dccf8d7008dd331cffd289f0454dc16e369
retvain <retvain@gmail.com>
2026-09-20 16:25:18 +0500

    Add Alt Arrow pane navigation

--- wezterm.lua
+++ wezterm.lua
@@ -8,6 +8,27 @@ config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe', '-NoLogo' 
 -- Alt+Shift+D: split the current pane and open PowerShell 7 in the new pane.
 -- `phys:D` refers to the physical D-key, so this works with a Russian layout too.
 config.keys = {
+  -- Alt+Arrow: move focus to an adjacent pane.
+  {
+    key = 'LeftArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'UpArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'DownArrow',
+    mods = 'ALT',
+    action = wezterm.action.ActivatePaneDirection 'Down',
+  },
   {
     key = 'phys:D',
     mods = 'ALT|SHIFT',
